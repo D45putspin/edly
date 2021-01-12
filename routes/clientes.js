5deeae70106f13d42262406760eb0cfb6df2f110
D45putspin <t14g0@protonmail.com>
2021-01-12 00:55:02 +0000

    progresso clientes

--- routes/clientes.js
+++ routes/clientes.js
@@ -18,8 +18,7 @@ router.get('/top_stores', login, async (req, res, next) => {
     var nomes = [];
     var ids = []
     const decode = jwt.verify(req.headers.token, "palavradificil");
-    var top1=0,top2=0,top3=0;
-    var idtop1,idtop2,idtop3;
+    
 
     database.all(sql, (err, rows) => {
         if (err) {
@@ -27,8 +26,9 @@ router.get('/top_stores', login, async (req, res, next) => {
         }
         if (rows) {
             rows.forEach((row) => {
-                console.log(row.Nome)
-            
+                
+            nomes.push(row.Nome);
+            ids.push(row.Id_loja)
 
 
             });
@@ -40,5 +40,36 @@ router.get('/top_stores', login, async (req, res, next) => {
     database.close();
     return
 });
+//function to get a rangom product image from a given store id
+router.get('/image/:id', login, async (req, res, next) => {
+    var database = new sqlite3.Database('edly.db', function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("OK");
+        }
+    });
+    let sql = `SELECT * FROM Produto  WHERE Id_loja = ? ORDER BY RANDOM () LIMIT 1 `;
+    var idrl = req.params.id;
+    var ids = idrl.replace("id=", "");
+    
+    database.all(sql,ids, (err, rows) => {
+        if (err) {
+            res.status(500).send({ error: "bd_error" })
+        }
+        if (rows) {
+            
+            rows.forEach((row) => {
+                console.log(row.image);
+                res.status(200).send({ nome_ficheiro:row.image})
 
+            });
+            
+           
+        }
+        else { res.status(404).send({ message: "No_registry" }) }
+    });
+    database.close();
+    return
+});
 module.exports = router;
