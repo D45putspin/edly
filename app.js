f6d46608f50e4759ef7ad0ea38ce70c78d656dd4
asdasd <asd>
2021-01-23 00:07:41 +0000

    app.js modif

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const rotaLojas = require('./routes/lojas');
 const rotaEncomendas= require('./routes/encomendas');
 const rotaAdmins= require('./routes/admins');
 const rotaEntregas= require('./routes/deliver');
-
+const path=require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false})); //apenas dados simples
 app.use(bodyParser.json());
 app.use(cors());
